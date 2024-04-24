79da6ce73ef9f8bf9d233ad2550eb34c978bd34a
necronomimicon <necronomimicon@gmail.com>
2024-04-24 09:10:17 +0200

    Update homeMigration.sh

--- homeMigration.sh
+++ homeMigration.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# read more about this script here:
+# https://gist.github.com/necronomimicon/075f56e62ca28c3b36b6794b755a211c
+
 # !! THIS WILL DELETE YOUR EXISTING $HOME FOLDERS AND ANYTHING INSIDE IT !!
 # !! MAKE SURE ALL THE IMPORTANT INFORMATION IS SAVED BEFOREHAND !!
 
